26e5bf6bac3f6da801a5caa1b9875504fba824de
FilipSwiatek <filipswiatek.fil@gmail.com>
2020-03-28 01:34:59 +0100

    dodano działającą mnożarkę (multiplier_rtl)

--- cordic_pipeline/cordic_pipeline.srcs/sources_1/new/elipse_cordic_rtl.sv
+++ cordic_pipeline/cordic_pipeline.srcs/sources_1/new/elipse_cordic_rtl.sv
@@ -21,6 +21,8 @@
 
 
 module elipse_cordic_rtl(a, b, clock, reset, ce, x, y, angle_in);
+    parameter FXP_MUL = 1024;
+    parameter FXP_POINT = 10;
     parameter integer W = 14; //Width of the fixed-point (15:10) representation
     input signed[W-1:0] angle_in, a, b;
     output signed[2*W-1:0] x, y;  
@@ -31,8 +33,8 @@ module elipse_cordic_rtl(a, b, clock, reset, ce, x, y, angle_in);
     //cordic processor
     cordic_pipe_rtl cordic_pipe_processor( .clock(clock), .reset(reset), .ce(ce), .angle_in(angle_in[11:0]), .sin_out(sin[11:0]), .cos_out(cos[11:0]));
     //multiplicators
-    multiplicator_rtl x_multiplicator(.a(a), .b(cos), .x(x_internal), .clock(clock), .ce(ce));
-    multiplicator_rtl y_multiplicator(.a(b), .b(sin), .x(y_internal), .clock(clock), .ce(ce));
+    multiplier_rtl x_multipliyer(.a(a), .b(cos), .x(x_internal), .clock(clock), .ce(ce));
+    multiplier_rtl y_multipliyer(.a(b), .b(sin), .x(y_internal), .clock(clock), .ce(ce));
     
     
     
@@ -42,7 +44,7 @@ module elipse_cordic_rtl(a, b, clock, reset, ce, x, y, angle_in);
     cos[W-1:12] = 1'b00;
     end
   
-  assign x = x_internal;
-  assign y = y_internal;
+  assign x = x_internal >>>FXP_POINT;
+  assign y = y_internal >>>FXP_POINT;
    
 endmodule //elipse_cordic_bahavioural
