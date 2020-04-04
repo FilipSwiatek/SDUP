184633435e517f3f5933501be5930549ce45a4da
FilipSwiatek <filipswiatek.fil@gmail.com>
2020-04-04 07:46:13 +0200

    konfiguracja projektu w SDK i dodanie maina

---
+++ elipse_acc_test/elipse_acc_test.sdk/elipse_test/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 4 Apr 2020
+ *      Author: Snickers
+ */
+
+#include "xparameters.h"
+#include "xgpio.h"
+#include "elipse_coprocessor.h"
+
+int main(void){
+
+
+	while(1); // infinite loop
+	return 0;
+}
