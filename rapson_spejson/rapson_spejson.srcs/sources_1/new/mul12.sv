c16bb006f657370417ba55f3d281a5ae900d8ec6
FilipSwiatek <filipswiatek.fil@gmail.com>
2020-04-06 19:28:37 +0200

    update o projekt Rapsona-spejsona

---
+++ rapson_spejson/rapson_spejson.srcs/sources_1/new/mul12.sv
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////
+// Filename: multiply12.sv
+// Module Name: mul12
+//////////////////////////////////////////////////////////////////////////////
+module mul12(
+input logic[12:0] input0,
+input logic[12:0] input1,
+output logic[24:0] output0
+);
+always_comb begin
+output0 = input0 * input1;
+end
+endmodule
