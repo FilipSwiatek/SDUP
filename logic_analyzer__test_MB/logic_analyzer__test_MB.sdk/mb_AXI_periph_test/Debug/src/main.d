d34fb7d100a79f3725772ad36a4f9d539b2f72f8
FilipSwiatek <filipswiatek.fil@gmail.com>
2020-08-25 18:57:32 +0200

    created SDK and written some code for MB (for now only typedefs and simmilar)

---
+++ logic_analyzer__test_MB/logic_analyzer__test_MB.sdk/mb_AXI_periph_test/Debug/src/main.d
@@ -0,0 +1,40 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xparameters.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/hi_speed_sampler.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_types.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xstatus.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_assert.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xgpio.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xgpio_l.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_io.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_printf.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xparameters.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/bspconfig.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/mb_interface.h \
+ ../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_exception.h
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xparameters.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/hi_speed_sampler.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_types.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xstatus.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_assert.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xgpio.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xgpio_l.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_io.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_printf.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xparameters.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/bspconfig.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/mb_interface.h:
+
+../../mb_AXI_periph_test_bsp/microblaze_0/include/xil_exception.h:
